7c4fd3d3d566494f0b3a099ce1bd2e74fd5ac753
JimmeeX <james.lin1997@hotmail.com>
2020-01-26 15:56:16 +1100

    refactor svg import

--- src/Home/image.js
+++ src/Home/image.js
@@ -1,83 +1,64 @@
 import React from 'react';
 
-import { ReactComponent as Boy } from '../images/home/boy.svg';
-import { ReactComponent as Browser } from '../images/home/monitor-browser-bw.svg';
-// import { ReactComponent as DataCloud } from '../images/home/data-cloud.svg';
-
-import { ReactComponent as Cloud } from '../images/home/cloud.svg';
-import { ReactComponent as Cog } from '../images/home/cog.svg';
-import { ReactComponent as Database } from '../images/home/database.svg';
-import { ReactComponent as LineChart } from '../images/home/line-chart.svg';
-import { ReactComponent as PieChart } from '../images/home/pie-chart.svg';
-
-
-import { useSpring, animated } from 'react-spring';
+import { useSpring, useSprings, animated } from 'react-spring';
+import SVG from 'react-inlinesvg';
 
 /**
  * TODO: ANIMATED TRAIL POP IN FOR ALL SVGS (TRY SCALE; HEIGHT)
- * TODO: SPIN COG
- * TODO: ANIMATE PIE CHART SEGMENT
  * TODO: ANIMATE POP IN WITH DURATION
 */
+
+const images = {
+  boy: { height: 300, top: 400, left: 150, delay: 1000 },
+  browser: { height: 200, top: 500, left: 300, delay: 6000 },
+  cloud: { height: 75, top: 465, left: 0, delay: 0 },
+  cog: { height: 45, top: 490, left: 70, delay: 0 },
+  database: { height: 150, top: 550, left: 0, delay: 0 }
+};
+
+const imageNames = Object.keys(images);
+
+const importFiles = (r) => {
+  const regex = /\.\/(.*)\.svg$/;
+  r.keys().map(key => {
+    const match = regex.exec(key);
+    const baseFile = match[1];
+    images[baseFile].src = r(key);
+    return null;
+  })
+};
+
+importFiles(require.context('../images/home', true, /\.svg$/));
 
 const Image = (props) => {
   const { duration } = props;
 
-  /**
-   * Boy Animation
-   */
+  const popSprings = useSprings(imageNames.length,
+    imageNames.map(name => ({
+      from: { height: '0px' },
+      to: { height: `${images[name].height}px` },
+      delay: images[name].delay
+    }))
+  );
 
-  const boySpring = useSpring({
-    from: {
-      height: '0px'
-    },
-    to: {
-      height: '300px',
-      // transform: 
-    },
-    // delay: 1000
-    config: { tension: 300, friction: 20 }
-  });
-
-  const browserSpring = useSpring({
-    from: { height: '0px' },
-    to: { height: '200px' },
-    // delay: duration * 2
-  });
-
-  const dataCloudSpring = useSpring({
-    from: { height: '0px' },
-    to: { height: '100px' }
-  });
-
-  const cogSpring = useSpring({
-    from: { height: '0px' },
-    to: { height: '50px' }
-  });
-
-  const AnimatedBoy = animated(Boy);
-  const AnimatedBrowser = animated(Browser);
-  // const AnimatedDataCloud = animated(DataCloud);
-
-  const AnimatedCloud = animated(Cloud);
-  const AnimatedCog = animated(Cog);
-  const AnimatedDatabase = animated(Database);
-  const AnimatedLineChart = animated(LineChart);
-  const AnimatedPieChart = animated(PieChart);
+  const AnimatedSVG = animated(SVG);
 
   return (
     <div className='image'>
-      <AnimatedBrowser style={browserSpring} />
-      {/* <AnimatedDataCloud style={dataCloudSpring} /> */}
-      <AnimatedCloud style={dataCloudSpring} />
-      <AnimatedCog style={cogSpring} />
-      <AnimatedDatabase style={dataCloudSpring} />
-      <AnimatedLineChart style={dataCloudSpring} />
-      <AnimatedPieChart style={dataCloudSpring} />
-
-
-      <AnimatedBoy style={boySpring} />
-      {/* <AnimatedBrowser style={boySpring} /> */}
+      {imageNames.map((name, i) => {
+        return (
+          <AnimatedSVG
+            src={images[name].src}
+            key={i}
+            style={{
+              ...popSprings[i],
+              position: 'absolute',
+              top: images[name].top,
+              left: images[name].left,
+            }}
+          />
+        )
+      })}
     </div>
   );
 };
