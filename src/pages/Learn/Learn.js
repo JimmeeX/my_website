54fae73d66bc93f2feeddbaddc89f0b9f3145e89
JimmeeX <james.lin1997@hotmail.com>
2020-01-29 03:48:39 +1100

    add learning & sharing

--- src/pages/Learn/Learn.js
+++ src/pages/Learn/Learn.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Button from '../../components/button';
+
 const Learn = () => {
   return (
-    <div id='learn'>
-      <div id='learn-grid'>
-        <h2>Learn & Share</h2>
-        <p>I love learning and sharing resources. You can check out what I know here. Learn and grow together as a community. Inspired by Nikita Voloboev</p>
+    <div id='learn' className='page-card'>
+      <div id='learn-text'>
+        <div id='learn-header' className='header-invert'>Learning & Sharing</div>
+        <p id='learn-paragraph' className='paragraph-invert'>I love learning and sharing resources. You can check out what I know here. Learn and grow together as a community. Inspired by Nikita Voloboev</p>
+        <Button text={'Checkout my Wiki'} />
       </div>
     </div>
   );
