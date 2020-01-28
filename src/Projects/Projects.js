b820467a8d0c3af9289c1810d29599ed57325b49
JimmeeX <james.lin1997@hotmail.com>
2020-01-28 11:16:45 +1100

    add projects layout

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Projects = () => {
   return (
     <div id='projects'>
-      <p>Projects :P</p>
+      <div id='projects-grid'>
+        <div id='projects-paper'>
+        </div>
+      </div>
     </div>
   );
 }
