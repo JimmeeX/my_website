9243ff2f1f9713be1275d3cb0da92ef39214ef91
JimmeeX <james.lin1997@hotmail.com>
2020-02-03 21:46:58 +1100

    add buildspec; add changes to intro text

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -5,7 +5,7 @@ const Intro = () => {
     <div id='intro' className='page-accent'>
       <div id='intro-text'>
         <div id='intro-header' className='header-invert'>Hello World! I'm James.</div>
-        <p id='intro-paragraph' className='paragraph-invert'>I am currently a final year Computer Science & Engineering student with a passion for App Development and Data Science. I also enjoy designing good UI/UX. I am currently looking for a full-time graduate position primarily in Full-stack Development to start in 2021.</p>
+        <p id='intro-paragraph' className='paragraph-invert'>I am currently a final year Computer Science & Engineering student with a passion for App Development and Data Science. I also enjoy designing good UI/UX. I am currently looking for a full-time graduate position or internship to jump-start my career in Full-stack Development.</p>
       </div>
     </div>
   );
